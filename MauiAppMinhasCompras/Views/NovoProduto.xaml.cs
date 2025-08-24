d795f9ecf86dfb4d7de4f3479b543c9e482fb8d1
lebandeira <lebandeira96@gmail.com>
2025-08-23 23:29:44 -0300

    Lista de Produtos e Adicionar novos produtos

--- MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
@@ -1,9 +1,30 @@
-namespace MauiAppMinhasCompras.Views;
+using System.Threading.Tasks;
+using MauiAppMinhasCompras.Models;
 
+namespace MauiAppMinhasCompras.Views;
 public partial class NovoProduto : ContentPage
 {
 	public NovoProduto()
 	{
 		InitializeComponent();
-	}
+	} 
+    private void ToolbarItem_Clicked_1(object sender, EventArgs e)
+    {
+        try
+        {
+            Produto p = new Produto
+            {
+                Descricao = txt_descricao.Text,
+                Quantidade = Convert.ToDouble(txt_quantidade.Text),
+                Preco = Convert.ToDouble(txt_preco.Text)
+            };
+            App.Db.Insert(p);
+            DisplayAlert("Sucesso!", "Registro Inserido", "OK");
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
 }
+
